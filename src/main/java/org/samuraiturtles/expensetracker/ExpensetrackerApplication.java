985764c8617f0f0062deb1d54cf0b9616f7896db
Douglas Domingos <doug.domingossilva@gmail.com>
2024-05-25 09:27:26 -0300

    Initial commit

---
+++ src/main/java/org/samuraiturtles/expensetracker/ExpensetrackerApplication.java
@@ -0,0 +1,21 @@
+package org.samuraiturtles.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ExpensetrackerApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/samuraiturtles/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samuraiturtles.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
